887fbc7af45198e9d401fa13b475b97cb6217e75
renlingxin <renlingxin@zuoyebang.com>
2020-10-14 12:01:57 +0800

    react

--- demo/react/one_test/src/App2.js
+++ demo/react/one_test/src/App2.js
@@ -6,21 +6,7 @@ import React, {
 // export default App2
 // 声明并且导出
 export class App2 extends Component {
-    componentDidMount(){
-        // 数据已经装载，据网上说可能会引起二次render
-        // 实际应用发网络请求
-        console.log('4:componentDidMount钩子函数');
-    }
-    componentWillMount(){
-        // 官方不推荐在此处发请求，可能会造成渲染的阻塞
-        console.log('2:componentWillMount钩子函数');
-    }
-    componentWillUpdate(){
-        console.log('componentwillupdate数据将要更改  钩子函数')
-    }
-    componentDidUpdate(){
-        console.log('componentDidUpdate数据已经更改 钩子函数')
-    }
+        
     constructor() {
         console.log('1:constructor钩子函数')
         super();
@@ -30,6 +16,31 @@ export class App2 extends Component {
         }
         // 初始化函数的指向 
         this.changehandler= this.changehandler.bind(this);
+    }
+    componentWillMount(){
+        // 一般用的比较少，它更多的是在服务端渲染时使用。它代表的过程是组件已经经历了constructor()初始化数据后，但是还未渲染DOM时。
+        console.log('2:componentWillMount钩子函数');
+    }
+    componentDidMount(){
+        // dom、数据已经装载，一般在这请求接口
+        console.log('4:componentDidMount钩子函数');
+    }
+    componentWillUnmount(){
+        // 组件销毁
+    }
+    componentWillReceiveProps(nextProps){
+    // 在接受父组件改变后的props需要重新渲染组件时用到的比较多
+    }
+    shouldComponentUpdate(nextProps,nextState){
+    // 用于控制组件渲染的声明周期   setState 后组件重新渲染 在这里return false 可以组织更新
+    }
+    componentWillUpdate(nextProps,nextState){
+        // shouldComponentUpdate 为true 后 进到这里
+        console.log('componentwillupdate数据将要更改  钩子函数')
+    }
+    componentDidUpdate(prevProps,prevState){
+        //组件渲染第一次会进到componentDidMount 之后的更新会进入到这里 prevProps prevState更新之前的数据
+        console.log('componentDidUpdate数据已经更改 钩子函数')
     }
     changehandler(e){
         console.log(e.target.value)
--- demo/算法/结构/table/index.js
+++ demo/算法/结构/table/index.js
@@ -21,6 +21,8 @@ class LinkedList {
     find(target) {
         let current = this.head
         while (current.element !== target) {
+            // 查不到就返回最后一个
+            if(!current.next) return current
             current = current.next
         }
         return current
@@ -29,14 +31,24 @@ class LinkedList {
     insert(newEle, oldEle) {
         let newNode = new Node(newEle);
         let oldNode = this.find(oldEle)
-        oldNode.next = newNode
+        // console.log(oldNode)
+        // 你可以注释了这行试试
+        if(oldNode.next === null){
+            oldNode.next =  newNode
+            return
+        }
+        // 注释了之后把下面两行换个位置
         newNode.next = oldNode.next
+        oldNode.next = newNode
     }
     // 显示链表中的元素
     display() {
         let current = this.head;
         while (current.element !== null) {
             console.log(current.element)
+            if(!current.next){
+                return
+            }
             current = current.next;
         }
     }
@@ -56,3 +68,30 @@ class LinkedList {
        }
     }
 }
+
+let t = new LinkedList()
+t.insert(1,'head')
+t.insert(2,1)
+t.insert(5,1)
+// t.insert(5,3)
+// t.display()
+
+console.log(t)
+
+
+// 给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+// 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+var swapPairs = function(head) {
+    if (head === null|| head.next === null) {
+        return head;
+    }
+    const newHead = head.next;
+    head.next = swapPairs(newHead.next);
+    newHead.next = head;
+    return newHead;
+};
+let res = swapPairs(t.head)
+// t.display()
+console.log(JSON.stringify(res))
