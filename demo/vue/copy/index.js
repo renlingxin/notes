163120614126ece286166c7f2d764b3cd57faf16
renlingxin <renlingxin@zuoyebang.com>
2020-12-18 20:57:46 +0800

    firday

--- demo/vue/copy/index.js
+++ demo/vue/copy/index.js
@@ -1,17 +1,23 @@
 class copyTest{
-    constructor(){
+    constructor({
+        btn
+    }){
         // super()
-
+        this.install(btn)
+        this.btnElement = null
     }
     // 初始化
-    install(vm,btn){
+    install(btn){
+        this.btnElement = document.querySelector(btn)
+        console.log('btnElement',this.btnElement);
         // vm.$copyText = this.$copyText
     }
     // 复制方法
     $copyText(va){
+        let _value = this.btnElement.value || va
         return new Promise((resolve,reject)=>{
             let _input = document.createElement('input')
-            _input.value = va
+            _input.value = _value
             document.body.appendChild(_input)
             _input.select()
             document.execCommand("Copy");
